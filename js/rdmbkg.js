e11db92d88ea4527a68c9856f5c5875c8cc19129
SealCoast <2784271883@qq.com>
2025-03-10 16:44:02 +0800

    Site updated: 2025-03-10 16:44:00

--- js/rdmbkg.js
+++ js/rdmbkg.js
@@ -1,7 +1,14 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-    "url(/img/banner/001.jpg)",
-    "url(/img/banner/002.jpg)"
+    "url(/img/background/01.jpg)",
+    "url(/img/background/02.jpg)",
+    "url(/img/background/03.jpg)",
+    "url(/img/background/04.jpg)",
+    "url(/img/background/10.jpg)",
+    "url(/img/background/23.jpg)",
+    "url(/img/background/39.jpg)",
+    "url(/img/background/42.jpg)",
+    "url(/img/background/55.jpg)"
   ];
   //获取背景图片总数，生成随机数
   var bgindex =Math.floor(Math.random() * (backimg.length));
@@ -10,8 +17,9 @@ var backimg =[
   
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
-    "url(/img/banner/001.jpg)",
-    "url(/img/banner/002.jpg)"
+    "url(/img/banner/Winslow.jpg)",
+    "url(/img/banner/Winslow.jpg)",
+    "url(/img/banner/Nakoa.jpg)"
   ];
   //获取banner图片总数，生成随机数
   var bannerindex =Math.ceil(Math.random() * (bannerimg.length-1));
